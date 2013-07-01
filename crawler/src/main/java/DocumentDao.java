4ce7810337279a48a94cf2d05fb87fda74dae3a7
huixiao200068 <huixiao200068@sohu-inc.com>
2013-07-01 11:51:02 +0800

    测试中文插入

--- crawler/src/main/java/DocumentDao.java
+++ crawler/src/main/java/DocumentDao.java
@@ -15,14 +15,10 @@ public class DocumentDao {
     private final static Sql2o sql2o = Executor.getSql2o();
 
     public static int insert(DocumentObj dobj) {
-//        String sql = "insert into document(id, title, desc, url) values (:id, :title, :desc, :url)";
-//        Connection connection = sql2o.createQuery(sql).addParameter("id", dobj.getId()).addParameter("title", dobj.getTitle())
-//                .addParameter("desc", dobj.getDesc()).addParameter("url", dobj.getUrl()).executeUpdate();
+        String sql = "insert into document(id, title, descr, url) values (:id, :title, :descr, :url)";
+        Connection connection = sql2o.createQuery(sql).addParameter("id", dobj.getId()).addParameter("title", dobj.getTitle())
+                .addParameter("descr", dobj.getDesc()).addParameter("url", dobj.getUrl()).executeUpdate();
 
-        String sql = "insert into document(id, title, desc, url) values ("+dobj.getId()+", '"+dobj.getTitle()+"', '"+dobj.getDesc()+"', '"+dobj.getUrl()+"')";
-        sql = "insert into document(id, title, desc, url) values (1,, '测试', '测试', '测试')";
-//        sql = Parser.encodeConvertor(sql, Charset.defaultCharset().name(), "UTF-8");
-        Connection connection = sql2o.createQuery(sql).executeUpdate();
         return connection.getResult();
     }
 }
