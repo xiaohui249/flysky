2a909889e3aa0a03eec2d5348ed8e5d74bd73dd3
xiaohui <huixiao200068@sohu-inc.com>
2013-07-04 13:30:00 +0800

    test

--- crawler/src/main/java/Crawler.java
+++ crawler/src/main/java/Crawler.java
@@ -5,6 +5,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import regex.RegExpValidator;
 import sequence.GenerateSequenceUtil;
 
