8e8a28eebf020b448370875ec68f5649b7948296
huixiao200068 <huixiao200068@sohu-inc.com>
2013-08-26 10:12:53 +0800

    Netty

---
+++ java/src/main/java/老师.java
@@ -0,0 +1,18 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: huixiao200068
+ * Date: 13-8-26
+ * Time: 上午10:08
+ * To change this template use File | Settings | File Templates.
+ */
+public class 老师 {
+    public void 讲课(){
+        System.out.println("同学们，今天考试");
+    }
+
+    public static void main(String[] 废话) {
+        老师 小高 = new 老师();
+        小高.讲课();
+    }
+
+}
