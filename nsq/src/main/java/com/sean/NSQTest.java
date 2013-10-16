067ab65a35791576f89353e82825d577994ca154
huixiao200068 <huixiao200068@sohu-inc.com>
2013-10-16 17:03:44 +0800

    sdf

--- nsq/src/main/java/com/sean/NSQTest.java
+++ nsq/src/main/java/com/sean/NSQTest.java
@@ -1,6 +1,7 @@
 package com.sean;
 
-import com.nsq.NSQProducer;
+import com.nsq.*;
+import com.nsq.lookup.NSQLookupDynMapImpl;
 
 /**
  * Created with IntelliJ IDEA.
@@ -10,16 +11,51 @@ import com.nsq.NSQProducer;
  * To change this template use File | Settings | File Templates.
  */
 public class NSQTest {
+
     public static void main(String[] args) throws Exception {
+//        producerStart();
+        consumerStart();
+    }
+
+    public static void producerStart() {
         NSQProducer producer = new NSQProducer().addAddress("10.10.76.98", 4150, 1);
         producer.start();
         String msg = "message";
-        for (int i = 0; i < 50000; i++) {
+        for (int i = 0; i < 10000; i++) {
             if (i % 1000 == 0) {
                 System.out.println("producer: " + i);
             }
-            producer.produceBatch("speedtest3", (msg + i).getBytes("utf8"));
+            try{
+                producer.produce("pub_term", (i + ":" + i).getBytes("utf8"));
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-
     }
+
+    public static void consumerStart() {
+
+        NSQLookup lookup = new NSQLookupDynMapImpl();
+        lookup.addAddr("10.10.76.98", 4161);
+        NSQConsumer consumer = new NSQConsumer(lookup, "pub_term", "channel1", new NSQMessageCallback() {
+            @Override
+            public void message(NSQMessage nsqMessage) {
+                try {
+                    nsqMessage.finished();
+                    String termInfo = new String(nsqMessage.getMessage(), "utf8");
+                    System.out.println(termInfo);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void error(Exception e) {
+                System.err.println("nsq error" + e.getMessage());
+            }
+        });
+        consumer.start();
+    }
+
+
 }
