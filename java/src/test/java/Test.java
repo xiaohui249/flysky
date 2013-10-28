ab6a34469c53bfdd0f2a91adb1019bbe6ca2a467
huixiao200068 <huixiao200068@sohu-inc.com>
2013-10-28 13:33:00 +0800

    add byte util

---
+++ java/src/test/java/Test.java
@@ -0,0 +1,47 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: huixiao200068
+ * Date: 13-10-28
+ * Time: 上午9:53
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+    public static void main(String[] args) {
+//        plus();
+        byte[] b = bytes(512);
+        int a = byteArray2int(b);
+    }
+
+    public static void plus() {
+        int i = 5;
+        for(int j=0; j<1000; j++) {
+            i = i++;
+        }
+        System.out.println(i);
+    }
+
+    public static byte[] bytes(int num) {
+        byte[] result = new byte[4];
+        result[0] = (byte)(num >>> 24);//取最高8位放到0下标
+        result[1] = (byte)(num >>> 16);//取次高8为放到1下标
+        result[2] = (byte)(num >>> 8); //取次低8位放到2下标
+        result[3] = (byte)(num );      //取最低8位放到3下标
+        return result;
+    }
+
+    public static int byteArray2int(byte[] b){
+        byte[] a = new byte[4];
+        int i = a.length - 1,j = b.length - 1;
+        for (; i >= 0 ; i--,j--) {//从b的尾部(即int值的低位)开始copy数据
+            if(j >= 0)
+                a[i] = b[j];
+            else
+                a[i] = 0;//如果b.length不足4,则将高位补0
+        }
+        int v0 = (a[0] & 0xff) << 24;//&0xff将byte值无差异转成int,避免Java自动类型提升后,会保留高位的符号位
+        int v1 = (a[1] & 0xff) << 16;
+        int v2 = (a[2] & 0xff) << 8;
+        int v3 = (a[3] & 0xff) ;
+        return v0 + v1 + v2 + v3;
+    }
+}
