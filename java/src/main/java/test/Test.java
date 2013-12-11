0dd887e8d27196a95218a361d4ec7236ff51a349
xiaohui <xiaohui_1123@126.com>
2013-12-11 23:29:28 +0800

    test

--- java/src/main/java/test/Test.java
+++ java/src/main/java/test/Test.java
@@ -9,18 +9,26 @@ package test;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println(DefaultClass.class);
+//        String str = new String("World");
+//        char ch[] = {'H', 'e', 'l', 'l', 'o'};
+//        change(str, ch);
+//        System.out.print(str + " and ");
+//        System.out.println(ch);
+//
+//        DefaultClass dc = new DefaultClass("a");
+//        System.out.println(dc.name);
+//        change(dc);
+//        System.out.println(dc.name);
 
-        String str = new String("World");
-        char ch[] = {'H', 'e', 'l', 'l', 'o'};
-        change(str, ch);
-        System.out.print(str + " and ");
-        System.out.println(ch);
+        int _i=1, _j=2;
+        change1(_i, _j);
+        System.out.println("i = " + _i + ", j = " + _j);
 
-        DefaultClass dc = new DefaultClass("a");
-        System.out.println(dc.name);
-        change(dc);
-        System.out.println(dc.name);
+        DefaultClass a = new DefaultClass("x");
+        DefaultClass b = new DefaultClass("y");
+        change2(a, b);
+        System.out.println("dc1.name = " + a.name + ", dc2.name = " + b.name);
+
     }
 
     public static void change(String str, char ch[]) {
@@ -34,5 +42,20 @@ public class Test {
         //下面也是引用传递
         DefaultClass _dc = dc;
         _dc.name = "b";
+    }
+
+    public static void change1(int i, int j) {
+        j = 0;
+        i = j;
+    }
+
+    /**
+     * 形参为复制的引用，即值传递，只是此处的值为引用值，即对象的内存地址。
+     * @param dc1
+     * @param dc2
+     */
+    public static void change2(DefaultClass dc1, DefaultClass dc2) {
+        dc2.name = "dc2";
+        dc1 = dc2;
     }
 }
