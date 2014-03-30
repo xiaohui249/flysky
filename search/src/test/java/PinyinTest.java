2befffe779b309361a4a91f05689241fde8b09e3
xiaohui <xiaohui_1123@126.com>
2014-03-31 00:45:04 +0800

    add pinyin test

---
+++ search/src/test/java/PinyinTest.java
@@ -0,0 +1,34 @@
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 14-3-31
+ * Time: 上午12:30
+ * To change this template use File | Settings | File Templates.
+ */
+public class PinyinTest {
+    public static void main(String[] args) throws Exception {
+        String host = "192.168.2.102";
+        ElasticSearchHandler esHandler = new ElasticSearchHandler(host);
+        String indexname = "medcl";
+        String type = "folks";
+
+        XContentBuilder jsonBuild = XContentFactory.jsonBuilder();
+        jsonBuild.startObject()
+                .field("id", "tom")
+                .field("name", "韩厚吉")
+                .endObject();
+        String jsonData = jsonBuild.string();
+        IndexResponse response = esHandler.createIndexResponse(indexname, type, jsonData);
+        if(response.isCreated()) {
+            System.out.println("创建成功！");
+        }
+    }
+}
