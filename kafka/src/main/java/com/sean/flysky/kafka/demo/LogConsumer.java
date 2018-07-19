65a104cad232457dfe5f038894b29ede0cf29321
xiaohui <xiaoh@kayak.com.cn>
2018-07-19 11:10:03 +0800

    完善kafka示例模块代码

--- kafka/src/main/java/com/sean/flysky/kafka/demo/LogConsumer.java
+++ kafka/src/main/java/com/sean/flysky/kafka/demo/LogConsumer.java
@@ -8,9 +8,10 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.*;
+import java.util.concurrent.*;
 
 /**
- * LogConsumer
+ * kafka消费者程序示例
  *
  * @author xiaoh
  * @create 2018-06-15 14:31
@@ -18,6 +19,8 @@ import java.util.*;
 public class LogConsumer {
     private final static Logger logger = LoggerFactory.getLogger(LogConsumer.class);
     private final static String CONSUMER_CONFIG_FILE = "consumer.properties";
+    private final static ExecutorService executorService = new ThreadPoolExecutor(100, 1000,
+            10, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>(1000));
 
     public static void main(String[] args) {
         Properties props = new Properties();
@@ -34,23 +37,59 @@ public class LogConsumer {
         long s = System.currentTimeMillis();
         List<ConsumerRecord<String, String>> buffer = new ArrayList<>();
         // 批量提交数量
-        final int minBatchSize = 1000;
-        while(true) {
-            ConsumerRecords<String, String> records = consumer.poll(
-                    Long.parseLong(props.getProperty("poll.timeout", "3000")));
-            if(!records.isEmpty()) {
-                for(ConsumerRecord<String, String> record : records) {
-//                    logger.info("key = " + record.key() + ", value = " + record.value());
-                    buffer.add(record);
-                    i++;
+        final int minBatchSize = Integer.parseInt(props.getProperty("minBatchSize", "1000"));
+        try {
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(
+                        Long.parseLong(props.getProperty("pollTimeout", "3000")));
+                if (!records.isEmpty()) {
+                    for (ConsumerRecord<String, String> record : records) {
+                        // 同步处理，如果处理过程复杂，阻塞offset的提交，整体消费处理性能缓慢，但是数据准确性和同步性较强
+                        syncDoWork(record);
+                        // 异步处理，无法处理是否成功，都将提交offset，整体消费处理性能快，但是处理失败的数据将丢失
+//                        asyncDoWork(record);
+
+                        // 写入内存队列，以便offset提交判断
+                        buffer.add(record);
+                        i++;
+                    }
+                    // 如果内存队列数据大于批量提交值，则提交offset
+                    if (buffer.size() > minBatchSize) {
+                        consumer.commitAsync();
+                        buffer.clear();
+                    }
+                } else {
+                    // 超过3秒没有消费到数据，则输出以下心跳日志，同时提交offset
+                    logger.info("consume {} messags, cost: {}ms, heartbeat...", i, System.currentTimeMillis() - s);
+                    if(buffer.size() > 0) {
+                        consumer.commitAsync();
+                        buffer.clear();
+                    }
                 }
-                if(buffer.size() > minBatchSize) {
-                    consumer.commitAsync();
-                    buffer.clear();
-                }
-            } else {
-                logger.info("consume {} messags, cost: {}ms, heartbeat...", i, System.currentTimeMillis() -s);
             }
+        }catch (Exception e) {
+            e.printStackTrace();
         }
+    }
+
+    /**
+     * 异步工作方法
+     * @param record
+     */
+    public static void asyncDoWork(final ConsumerRecord<String, String> record) {
+        executorService.submit(new Runnable() {
+            @Override
+            public void run() {
+                logger.info("key = " + record.key() + ", value = " + record.value());
+            }
+        });
+    }
+
+    /**
+     * 同步工作方法
+     * @param record
+     */
+    public static void syncDoWork(ConsumerRecord<String, String> record) {
+        logger.info("key = " + record.key() + ", value = " + record.value());
     }
 }
--- kafka/src/main/java/com/sean/flysky/kafka/demo/LogProducer.java
+++ kafka/src/main/java/com/sean/flysky/kafka/demo/LogProducer.java
@@ -9,7 +9,7 @@ import java.util.Properties;
 import java.util.concurrent.Future;
 
 /**
- * LogProducer
+ * Kafka生产者程序示例
  *
  * @author xiaoh
  * @create 2018-06-19 17:16
@@ -27,14 +27,23 @@ public class LogProducer {
             logger.error("Failed to load the config file of kafka producer!");
         }
 
-        String topic = "mytopic";
-        notAlwaysProduce(props, topic);
+        String topic = props.getProperty("topic", "");
+        if(topic.trim().length() == 0) {
+            logger.error("Please set topic!");
+            System.exit(0);
+        }
+        alwaysProduce(props, topic);
     }
 
+    /**
+     * 定量生产数据，以便性能测试
+     * @param props
+     * @param topic
+     */
     public static void notAlwaysProduce(Properties props, String topic) {
         Producer<String, String> producer = new KafkaProducer<>(props);
         long s = System.currentTimeMillis();
-        int x = 0, y = 100000;
+        int x = 0, y = 500000;
         for(int i=x; i<y; i++) {
             final String value = "message" + i;
             Future<RecordMetadata> future = producer.send(new ProducerRecord<String, String>(topic, value), new Callback() {
@@ -43,17 +52,22 @@ public class LogProducer {
                     if(null != exception) {
                         exception.printStackTrace();
                     } else {
-//                        logger.info(value + ", offset: " + metadata.offset());
+                        logger.info(value + ", offset: " + metadata.offset());
                     }
                 }
             });
-//            RecordMetadata metadata = future.get();
+//            RecordMetadata metadata = future.get();   // 同步生产
 //            logger.info("log" + i + ", offset: " + metadata.offset());
         }
         logger.info("produce {} messages, cost {}ms", y-x, System.currentTimeMillis() - s);
         producer.close();
     }
 
+    /**
+     * 持续一直生产数据
+     * @param props
+     * @param topic
+     */
     public static void alwaysProduce(Properties props, String topic) {
         Producer<String, String> producer = new KafkaProducer<>(props);
         int i = 1;
@@ -70,6 +84,11 @@ public class LogProducer {
                 }
             });
             i++;
+            try {
+                Thread.sleep(1);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
